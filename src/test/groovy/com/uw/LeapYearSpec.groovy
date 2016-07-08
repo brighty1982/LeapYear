90adf9e44f635d356d422e8acb584ffeaddacdc0
brighty1982 <ambright_1@hotmail.com>
2016-07-08 15:43:19 +0100

    Initial commit

---
+++ src/test/groovy/com/uw/LeapYearSpec.groovy
@@ -0,0 +1,59 @@
+package com.uw
+
+import spock.lang.Specification
+
+/**
+ * Created by abright on 08/07/2016.
+ */
+class LeapYearSpec extends Specification{
+
+    def "Value divided by 4 is Leap Year"(){
+
+        given:
+        def year = 1996
+
+        when:
+        LeapYear leapYear = new LeapYear(year)
+
+        then:
+        leapYear.isLeap == true
+
+    }
+
+    def "Value divided by 100 is not a Leap Year"(){
+
+        given:
+        def year = 1900
+
+        when:
+        LeapYear leapYear = new LeapYear(year)
+
+        then:
+        leapYear.isLeap == false
+    }
+
+    def "Value divided by 400 is a Leap Year"(){
+
+        given:
+        def year = 2000
+
+        when:
+        LeapYear leapYear = new LeapYear(year)
+
+        then:
+        leapYear.isLeap == true
+    }
+
+    def "Value is not divisble by 4 then it is not a Leap Year"(){
+
+        given:
+        def year = 2001
+
+        when:
+        LeapYear leapYear = new LeapYear(year)
+
+        then:
+        leapYear.isLeap == false
+    }
+
+}
