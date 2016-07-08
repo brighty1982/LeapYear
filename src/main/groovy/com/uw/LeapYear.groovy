e876e0a667e0fd91ee3f109b9f76dea294954c17
brighty1982 <ambright_1@hotmail.com>
2016-07-08 15:46:05 +0100

    Upload remaining files

---
+++ src/main/groovy/com/uw/LeapYear.groovy
@@ -0,0 +1,27 @@
+package com.uw
+
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+/**
+ * Created by abright on 08/07/2016.
+ */
+class LeapYear {
+
+    Boolean isLeap
+
+    LeapYear(def year){
+        isLeap = isDiv400(year) || (isDiv4(year) && !isDiv100(year))
+    }
+
+    Boolean isDiv4(def year){
+        year % 4 == 0
+    }
+
+    Boolean isDiv100(def year){
+        year % 100 == 0
+    }
+
+    Boolean isDiv400(def year){
+        year % 400 == 0
+    }
+}
